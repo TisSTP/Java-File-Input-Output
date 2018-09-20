80e937b7f26f3a2a0a3e4297ea5d04c1ddfeb8c4
Sathaphorn Sunthornpan <sathaphorn.stp@gmail.com>
2018-09-21 01:48:33 +0700

    File: Buffered And Unbuffered

---
+++ src/com/stp/BufferedMain.java
@@ -0,0 +1,67 @@
+package com.stp;
+
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.WRITE;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+
+/**
+ * @author sathaphorn.stp
+ * @since 21-09-2018, 00:36
+ */
+public class BufferedMain {
+
+  public static void main(String[] args) {
+    Charset charset = Charset.forName("TIS-620"); // Encoding: ANSI
+    Charset charset2 = Charset.forName("UTF-8"); // Encoding: UTF-8
+    File file = new File("resource/source.txt");
+    File file2 = new File("resource/target.txt");
+    // Path path = Paths.get(file.getPath());
+
+    if (file.isFile()) {
+      // Read File
+      try (BufferedReader reader = Files.newBufferedReader(file.toPath(), charset)) {
+        String line = null;
+        try (BufferedWriter writer = Files.newBufferedWriter(file2.toPath(), charset2, CREATE, WRITE, APPEND)) {
+          // Read
+          boolean isNewLine = false;
+          while ((line = reader.readLine()) != null) {
+
+            // Enter Line: No Enter Line; Windows(CR LF), Mac(CR), Unix(LF)
+            if (isNewLine) {
+              writer.newLine();
+              // writer.write(line + "\r\n");
+            }
+
+            // Write
+            writer.write(line, 0, line.length());
+            System.out.println(line);
+
+            isNewLine = true;
+          }
+
+        } catch (IOException x) {
+          System.err.format("IOException Writer: %s%n", x);
+        }
+
+      } catch (IOException x) {
+        System.err.format("IOException Reader: %s%n", x);
+      }
+
+    } else {
+      // New File
+      String s = "";
+      try (BufferedWriter writer = Files.newBufferedWriter(file2.toPath(), charset, CREATE)) {
+        writer.write(s, 0, s.length());
+      } catch (IOException x) {
+        System.err.format("IOException Writer: %s%n", x);
+      }
+    }
+  }
+}
---
+++ src/com/stp/UnbufferedMain.java
@@ -0,0 +1,61 @@
+package com.stp;
+
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * @author sathaphorn.stp
+ * @since 21-09-2018, 00:37
+ */
+public class UnbufferedMain {
+
+  public static void main(String[] args) {
+    Charset charset = Charset.forName("TIS-620"); // Encoding: ANSI
+    Charset charset2 = Charset.forName("UTF-8"); // Encoding: UTF-8
+    File file = new File("resource/source.txt");
+    File file2 = new File("resource/target.txt");
+    // Path path = Paths.get(file.getPath());
+
+    if (file.isFile()) {
+      // Read File
+      try {
+        FileInputStream inputStream = new FileInputStream(file);
+        InputStreamReader reader = new InputStreamReader(inputStream, charset);
+        int character;
+
+        try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file2), charset2)) {
+          // Read
+          while ((character = reader.read()) != -1) {
+            writer.write((char) character);
+            System.out.print((char) character);
+          }
+        }
+
+        reader.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+    } else {
+      // New File
+
+    }
+  }
+}
